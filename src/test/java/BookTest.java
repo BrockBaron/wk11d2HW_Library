d96b03a4132d19f430618674feacf7e550c81a04
BrockBaron <Nslekkas@gmail.com>
2021-05-18 20:14:27 +0100

    book test initiated

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+    Book book;
+
+    @Before
+    public void before(){
+        book = new Book("Jurassic Park", "Michael Crichton", "Sci-fi");
+    }
+
+    @Test
+    public void bookHasTitle() {
+        assertEquals("Jurrasic Park", book.getTitle());
+    }
+
 }
