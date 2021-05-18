5b1fe5cbda2ee1da6804b4a3b05c8e317527344a
BrockBaron <Nslekkas@gmail.com>
2021-05-18 20:05:41 +0100

    Library class added

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> bookStock;
+
+    public Library(){
+        this.bookStock = new ArrayList<>();
+    }
+
+
+
+
 }
