6ec9a15242951563a7579cf34f121f25b6bfe729
BrockBaron <Nslekkas@gmail.com>
2021-05-18 20:34:22 +0100

    LibraryTest test setup

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+
 public class LibraryTest {
+    Library library;
+    Book book;
+    Book book1;
+    Book book2;
+    Book book3;
+    Book book4;
+
+    @Before
+    public void before(){
+        library = new Library();
+        book = new Book("Jurassic Park", "Michael Crichton", "Sci-fi");
+        book1 = new Book("Martian Chronicles", "Ray Bradbury", "Sci-fi");
+        book2 = new Book("Black Box Thinking", "Matthew Syed", "Self-help");
+        book3 = new Book("Dune", "Frank Herbert", "Sci-fi");
+        book4 = new Book("Howl's Moving Castle", "Diana Wynne Jones", "Fantasy");
+
+    }
+
+
+
+//    @Test
 }
